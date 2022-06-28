c9b51ce28bf2b9904ffbbedf824790073ec75a03
kazaryanv <kazaryanv01@mail.ru>
2022-06-28 22:46:50 +0300

    edit

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::prefix('admin')->group(function () {
     Route::get("company/panel", [CompanyController::class, 'index'])->name("company_panel");
     Route::get('company/panel/new', [CompanyController::class, 'create'])->name('new-company');
     Route::post('company/panel/new/send', [CompanyController::class, 'store'])->name('store-company');
-    Route::get('company/panel/new/send{id}', [CompanyController::class, 'show'])->name('show-company');
-    Route::get('admin/company/panel/edit/{id}', [CompanyController::class, 'edit'])->name('edit-company');
-    Route::post('admin/company/panel/edit/store', [CompanyController::class, 'update'])->name('update-company');
-    Route::get('/admin/company/panel/delete/{id}',[CompanyController::class,'destroy'])->name('delete-company');
+    Route::get('company/panel/new/send/=/{id}', [CompanyController::class, 'show'])->name('show-company');
+    Route::get('/company/panel/edit/{id}', [CompanyController::class, 'edit'])->name('edit-company');
+    Route::post('/company/panel/edit/store', [CompanyController::class, 'update'])->name('update-company');
+    Route::get('/company/panel/delete/{id}',[CompanyController::class,'destroy'])->name('delete-company');
+
+
+    Route::resource("/employee", EmployeeController::class);
 });
 
-Route::resource("/employee", EmployeeController::class);
